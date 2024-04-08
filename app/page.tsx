b84c0cb59c6139e1e44fe942f17b8c6a2a543534
Project SURVIVOR <161699187+olawaleraji@users.noreply.github.com>
2024-04-08 12:48:16 +0100

    check page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Navbar from './NavbarOne'
-import NavbarThree from './NavbarThree'
-import NavbarTwo from './NavbarTwo'
+import React from "react";
+import Navbar from "./NavbarOne";
+import NavbarThree from "./NavbarThree";
+import NavbarTwo from "./NavbarTwo";
 
-type Props = {}
+type Props = {};
 
 const page = (props: Props) => {
   return (
@@ -11,8 +11,9 @@ const page = (props: Props) => {
       <Navbar />
       {/* <NavbarTwo/> */}
       {/* <NavbarThree /> */}
+      https://www.figma.com/file/uWdpMlKhbhg6slszSSNmmg/Floopyinn-Rental-Website-Ui--kit-(Community)?type=design&node-id=1-20&mode=design&t=VYDRCSkKbiz4ttr3-0
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
